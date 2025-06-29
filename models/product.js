354aa3826204fe1968503591baf52ed08b1081fb
Kayode <drkay4sure@gmail.com>
2025-06-29 23:01:55 +0100

    done with product schema

--- models/product.js
+++ models/product.js
@@ -1,7 +1,30 @@
 const mongoose = require('mongoose');
 
-const productSchema = new mongoose.Schema({
-
+const ProductSchema = new mongoose.Schema({
+    seller: {
+        type: String,
+        required : [true, 'Name of seller is required'],
+        trim : true,
+        maxLength: [50, 'Seller name can not be more than 50 characters']
+    },
+    product: {
+       type: String,
+        required : [true, 'Name of product is required'],
+        trim : true, 
+        maxLength: [50, 'Product name can not be more than 50 characters']
+    },
+    cost: {
+        type: Number,
+        required : [true, 'Cost of product is required']
+    },
+    quantity: {
+        type: Number,
+        required: [true, 'The quntity of product available is required']
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    }
 })
 
-module.exports = productSchema;
+module.exports = mongoose.model('Product', productSchema)
